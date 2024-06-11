95837618f209b3655c30539d39772dce0d44be24
mohammadsulaiman29 <mohammadsulaiman355@github.com>
2024-06-11 15:13:40 +0300

    Add functional components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Greet , Welcome} from './components/Greet'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet/>
+      <Welcome/>
     </div>
   );
-}
-
-function MyTest(){
-  return (
-    <div>
-      Hello mohammad;
-    </div>
-  )
 }
 
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css'
+// function Greet(){
+//     return <h1>Hello Mohammad</h1>
+// }
+
+export const Greet = () => {
+    return <h1 className="Greet">Hello mohammad</h1>
+}
+
+export const Welcome = () => {
+    return <a href="">Welcome</a>
+}
+// export default  Greet;
